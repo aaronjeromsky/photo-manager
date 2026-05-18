dae541815aa52b9846b7fd11c7c2925c5bce17be
Aaron Jeromsky <aaron.k.jeromsky@gmail.com>
2026-05-18 00:07:07 -0700

    feat: Initialize Spring Boot project

---
+++ src/test/java/io/github/aaronjeromsky/photomanager/PhotoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aaronjeromsky.photomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
